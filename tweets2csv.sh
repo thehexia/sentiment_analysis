b515f013c389232c71dd7d5b3b1a5eb15f0e5fcc
Hoang Nguyen <hvn1@hvn1-Inspiron-N5040.(none)>
2014-11-09 13:41:31 -0500

    Finished tweets2csv.sh

--- tweets2csv.sh
+++ tweets2csv.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-python tweets2csv.py tweets/EST_tweets/
-python tweets2csv.py tweets/PST_tweets/
-python tweets2csv.py tweets/CT_tweets/
+# python tweets2csv.py tweets/EST_tweets/
+# python tweets2csv.py tweets/PST_tweets/
+# python tweets2csv.py tweets/CT_tweets/
 
 echo "tweets/EST_tweets/by_day/\n";
 for dir in `ls tweets/EST_tweets/by_day/`;
 do
 	echo "tweets/EST_tweets/by_day/"$dir"/"
 	python tweets2csv.py "tweets/EST_tweets/by_day/"$dir"/"
-	cat "tweets/EST_tweets/by_day/"$dir"/csv/"* > "tweets/EST_tweets/by_day/"$dir"/csv/"merge.csv;
+	cat "tweets/EST_tweets/by_day/"$dir"/csv/"*.csv > "tweets/EST_tweets/by_day/"$dir"/csv/"merge.csv;
 done
 
 echo "tweets/EST_tweets/by_hour/\n";
@@ -17,7 +17,7 @@ for dir in `ls tweets/EST_tweets/by_hour/`;
 do
 	echo "tweets/EST_tweets/by_hour/"$dir"/"
 	python tweets2csv.py "tweets/EST_tweets/by_hour/"$dir"/"
-	cat "tweets/EST_tweets/by_hour/"$dir"/csv/"* > merge.csv;
+	cat "tweets/EST_tweets/by_hour/"$dir"/csv/"*.csv  > merge.csv;
 done
 
 echo "tweets/PST_tweets/by_day/\n";
@@ -25,7 +25,7 @@ for dir in `ls tweets/PST_tweets/by_day/`;
 do
 	echo "tweets/PST_tweets/by_day/"$dir"/"
 	python tweets2csv.py "tweets/PST_tweets/by_day/"$dir"/"
-	cat "tweets/PST_tweets/by_day/"$dir"/csv/"* > merge.csv;
+	cat "tweets/PST_tweets/by_day/"$dir"/csv/"*.csv  > merge.csv;
 done
 
 echo "tweets/PST_tweets/by_hour/\n";
@@ -33,7 +33,7 @@ for dir in `ls tweets/PST_tweets/by_hour/`;
 do
 	echo "tweets/PST_tweets/by_hour/"$dir"/"
 	python tweets2csv.py "tweets/PST_tweets/by_hour/"$dir"/"
-	cat "tweets/PST_tweets/by_hour/"$dir"/csv/"* > merge.csv;
+	cat "tweets/PST_tweets/by_hour/"$dir"/csv/"*.csv  > merge.csv;
 done
 
 echo "tweets/CT_tweets/by_day/\n";
@@ -41,7 +41,7 @@ for dir in `ls tweets/CT_tweets/by_day/`;
 do
 	echo "tweets/CT_tweets/by_day/"$dir"/"
 	python tweets2csv.py "tweets/CT_tweets/by_day/"$dir"/"
-	cat "tweets/CT_tweets/by_day/"$dir"/csv/"* > merge.csv;
+	cat "tweets/CT_tweets/by_day/"$dir"/csv/"*.csv  > merge.csv;
 done
 
 echo "tweets/CT_tweets/by_hour/\n"
@@ -49,5 +49,5 @@ for dir in `ls tweets/CT_tweets/by_hour/`;
 do
 	echo "tweets/CT_tweets/by_hour/"$dir"/"
 	python tweets2csv.py "tweets/CT_tweets/by_hour/"$dir"/"
-	cat "tweets/CT_tweets/by_hour/"$dir"/csv/"* > merge.csv
+	cat "tweets/CT_tweets/by_hour/"$dir"/csv/"*.csv  > merge.csv
 done
